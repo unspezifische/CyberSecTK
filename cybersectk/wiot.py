0173d36829000d6c94c0e3de37ea7ef538f1adff
unspezifische <unspezifische@gmail.com>
2024-01-23 14:49:55 -0600

    updated `wiot.py` to include a namespace main line

--- cybersectk/wiot.py
+++ cybersectk/wiot.py
@@ -1,18 +1,22 @@
 import scapy.all as scapy
+import numpy as np
 
-###################################################
-#################### WLAN IOT #####################
-f = open ("IOTwireless.csv", "w")
-f.writelines("version,Pad,Len,Rate,ChannelFrequency,ChannelFlags,dBm_AntSignal,Antenna,subtype,\
-type,proto,FCfield,ID,addr1,addr2,addr3,SC,addr4,Dot11Elt1.ID,Dot11Elt1.len,Dot11Elt1.info\n")
 def wiot(frame):
+    data = []
     if frame.haslayer(scapy.Dot11):
         for packets in frame:
-            f.writelines('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,'%(frame.version,frame.pad,frame.len,\
-                    frame.Rate,frame.ChannelFrequency,frame.ChannelFlags,frame.dBm_AntSignal,frame.Antenna,\
-                    frame.subtype,frame.type,frame.proto,frame.FCfield,frame.ID,frame.addr1,frame.addr2,frame.addr3,frame.SC,frame.addr4))
+            row = [frame.version, frame.pad, frame.len, frame.Rate, frame.ChannelFrequency, frame.ChannelFlags, 
+                   frame.dBm_AntSignal, frame.Antenna, frame.subtype, frame.type, frame.proto, frame.FCfield, 
+                   frame.ID, frame.addr1, frame.addr2, frame.addr3, frame.SC, frame.addr4]
             if packets.haslayer(scapy.Dot11Elt):
-                packets=scapy.Dot11Elt()
-                f.writelines('%s,%s,%s,'%(frame.payload.ID,frame.payload.len,frame.info.decode()))
-            f.writelines('\n')
-scapy.sniff(offline=input("Enter the Pcap file:"), prn=wiot)
+                packets = scapy.Dot11Elt()
+                row.extend([frame.payload.ID, frame.payload.len, frame.info.decode()])
+            data.append(row)
+    return np.array(data)
+
+if __name__ == "__main__":
+    pcap_file = input("Enter the Pcap file:")
+    frames = scapy.sniff(offline=pcap_file)
+    for frame in frames:
+        data = wiot(frame)
+        print(data)
